21162770c4daa311d13008b4573e8129633e77c2
schlagercollin <schlagercollin@gmail.com>
2016-12-29 22:59:14 -0700

    Filter works! Still some bugs though
    The WP category filter works. Issue was finding the right way to query
    through WP-API and ember data.
    
    Minor issue remains in that the WP category array is displayed as a
    string with a comma (at least I believe). Thus, because our two
    questions have categories 2,5 and 4,2, entering 2 into the input field
    will return both questions. A solution would be implementing a
    transformer that allows us to get actual arrays from ember data so that
    we can fine tune our filter to recognize number order (category and
    subcategory).
    
    I have found a possible transformer that will do just this. Will check
    back later.

--- app/models/question.js
+++ app/models/question.js
@@ -5,7 +5,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   answer: DS.attr('number'),
   direction: DS.attr('string'),
-  categoryTest: DS.attr('string'), //Doesn't seem to work?
+  categories: DS.attr(),
   option1: DS.attr(),
   option2: DS.attr(),
   option3: DS.attr(),
