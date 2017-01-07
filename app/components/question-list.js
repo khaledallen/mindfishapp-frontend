78f05e7c3b976f14530bf5b96552724a8047db4d
Khaled Allen <khaled.allen@gmail.com>
2017-01-06 23:36:06 -0500

    Handling the question options as an array.

--- app/components/question-list.js
+++ app/components/question-list.js
@@ -4,21 +4,38 @@ export default Ember.Component.extend({
   isCorrect: false,
   isIncorrect: false,
   currentAnswer: null,
+  option1: null,
+  option2: null,
+  option3: null,
+  option4: null,
+
+  init () {
+    this._super(...arguments);
+    this.setup();
+  },
+
   actions: {
     setAnswer(answer) {
       this.set('currentAnswer', answer);
-      console.log(answer);
     },
+
     checkCorrect(check) {
-      if (this.currentAnswer == check) {
+      if (this.currentAnswer === check) {
         this.set('isCorrect',true);
         this.set('isIncorrect',false);
-      } else {this.set('isCorrect',false);this.set('isIncorrect',true);}
-      console.log(this.isCorrect);
-      console.log(this.isIncorrect);
-      console.log(check);
+        } else {
+          this.set('isCorrect',false);
+          this.set('isIncorrect',true);
+        };
+    }
+  },
+
+    setup() {
+      this.set('option1', this.options[0]);
+      this.set('option2', this.options[1]);
+      this.set('option3', this.options[2]);
+      this.set('option4', this.options[3]);
+//TODO Make this a loop so it accounts for any size array
     }
 
-
-  }
 });
--- app/models/question.js
+++ app/models/question.js
@@ -1,9 +1,10 @@
 import DS from 'ember-data';
+import PostModel from 'ember-wordpress/models/post';
 
-export default DS.Model.extend({
+export default PostModel.extend({
   answer: DS.attr(),
   direction: DS.attr('string'),
-  categories: DS.attr(),
+  options: DS.attr(),
   option1: DS.attr(),
   option2: DS.attr(),
   option3: DS.attr(),
