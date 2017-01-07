27cf66ba2d21b4f1ad898ace5b64e8aaa5cd9532
Khaled Allen <khaled.allen@gmail.com>
2017-01-06 23:48:52 -0500

    Answer options generated by iterating the array

--- app/components/question-list.js
+++ app/components/question-list.js
@@ -4,14 +4,9 @@ export default Ember.Component.extend({
   isCorrect: false,
   isIncorrect: false,
   currentAnswer: null,
-  option1: null,
-  option2: null,
-  option3: null,
-  option4: null,
 
   init () {
     this._super(...arguments);
-    this.setup();
   },
 
   actions: {
@@ -26,16 +21,8 @@ export default Ember.Component.extend({
         } else {
           this.set('isCorrect',false);
           this.set('isIncorrect',true);
-        };
+        }
     }
   },
-
-    setup() {
-      this.set('option1', this.options[0]);
-      this.set('option2', this.options[1]);
-      this.set('option3', this.options[2]);
-      this.set('option4', this.options[3]);
-//TODO Make this a loop so it accounts for any size array
-    }
 
 });
