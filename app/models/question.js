5a071f0f292bb0f161623a88bac84ee6aa0a871e
Khaled Allen <khaled.allen@gmail.com>
2017-01-09 12:17:35 -0500

    Setting up the authorization branch and tools

--- app/models/question.js
+++ app/models/question.js
@@ -5,8 +5,4 @@ export default PostModel.extend({
   answer: DS.attr(),
   direction: DS.attr('string'),
   options: DS.attr(),
-  option1: DS.attr(),
-  option2: DS.attr(),
-  option3: DS.attr(),
-  option4: DS.attr(),
 });
